8b3926909bf0eb53dbd1b312bcbc81340932083b
Martino Mensio <martino.mensio@open.ac.uk>
2019-01-24 14:58:04 +0000

    adding animation

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { APIService, CountResult } from '../../api.service';
+import { trigger, style, transition, animate, keyframes, query, stagger, state } from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  animations: [
+    trigger(
+      'enterAnimation', [
+        transition(':enter', [
+          style({transform: 'translateX(100%)', opacity: 0}),
+          animate('500ms', style({transform: 'translateX(0)', opacity: 1}))
+        ]),
+        transition(':leave', [
+          style({transform: 'translateX(0)', opacity: 1}),
+          animate('500ms', style({transform: 'translateX(100%)', opacity: 0}))
+        ])
+      ]
+    )
+  ]
 })
 export class HomeComponent implements OnInit {
 
