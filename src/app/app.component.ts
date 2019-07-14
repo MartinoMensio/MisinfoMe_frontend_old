05e23ba78f95855fb3f65633e4b11324969de95f
Martino Mensio <martino.mensio@open.ac.uk>
2019-07-14 17:15:40 +0100

    google analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+
+// declare ga as a function to set and sent the events
+declare let ga: Function;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+
+  constructor(public router: Router) {
+
+    // subscribe to router events and send page views to Google Analytics
+    this.router.events.subscribe(event => {
+
+      if (event instanceof NavigationEnd) {
+        ga('set', 'page', event.urlAfterRedirects);
+        ga('send', 'pageview');
+
+      }
+
+    });
+  }
 }
