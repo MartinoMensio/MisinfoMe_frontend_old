ffced4ba345c291d18867d2b94d025f6c4c709da
Martino Mensio <martino.mensio@open.ac.uk>
2019-08-21 18:17:15 +0100

    fade to gray accordingly to inconfidence

--- src/app/components/credibility-meter/credibility-meter.component.ts
+++ src/app/components/credibility-meter/credibility-meter.component.ts
@@ -40,23 +40,40 @@ export class CredibilityMeterComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const red = this._confidence_fade_to_grey(this._red_quantity(this.credibility.value), this.credibility.confidence);
+    const green = this._confidence_fade_to_grey(this._green_quantity(this.credibility.value), this.credibility.confidence);
+    const blue = this._confidence_fade_to_grey(0, this.credibility.confidence, true);
+    const rgb_str = `rgb(${red},${green},${blue})`;
+    console.log(rgb_str)
     this.thresholdConfig = {
-      '-1': {'color': `rgb(${this._red_quantity(this.credibility.value)},${this._green_quantity(this.credibility.value)},0)`}
-    }
+      '-1': {'color': rgb_str}
+    };
   }
 
   _red_quantity(credibility_value) {
+    let red = 200;
     if (credibility_value > 0) {
-      return (1 - credibility_value) * 200;
+      red = (1 - credibility_value) * 200;
     }
-    return 200;
+    return red;
   }
 
   _green_quantity(credibility_value) {
+    let green = 200;
     if (credibility_value < 0) {
-      return (1 +credibility_value) * 200;
+      green = (1 + credibility_value) * 200;
     }
-    return 200;
+    return green;
+  }
+
+  _confidence_fade_to_grey(value, confidence, opposite = false) {
+    const uncertainty = 1 - confidence;
+    if (opposite || !opposite) {
+      // how far from 200
+      const gap = 200 - value;
+      const bonus = gap * uncertainty;
+      return value + bonus;
+    }
   }
 
 }
