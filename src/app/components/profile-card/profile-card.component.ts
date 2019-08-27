4f5eb4034009cf08f9b72efc1b1e04a812550007
Martino Mensio <martino.mensio@open.ac.uk>
2019-08-27 17:14:48 +0100

    sort sources by credibility value

--- src/app/components/profile-card/profile-card.component.ts
+++ src/app/components/profile-card/profile-card.component.ts
@@ -7,8 +7,18 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ProfileCardComponent implements OnInit {
 
+  _profileAssessment: any;
   @Input()
-  profileAssessment: any;
+  set profileAssessment(profileAssessment) {
+    this._profileAssessment = profileAssessment;
+    this.profileAssessment.assessments = this.profileAssessment.assessments.sort((e1, e2) => {
+      // (e2.credibility.confidence - e1.credibility.confidence) +
+      return (e1.credibility.value - e2.credibility.value);
+    });
+  }
+  get profileAssessment() {
+    return this._profileAssessment;
+  }
 
   constructor() { }
 
