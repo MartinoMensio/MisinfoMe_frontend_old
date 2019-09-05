0807f08cbec885bf9be52aff9d7c171867766323
Martino Mensio <martino.mensio@open.ac.uk>
2019-09-05 18:04:04 +0100

    sizing and adapting flow graph

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -11,6 +11,14 @@ export class AboutComponent implements OnInit {
   about_stats = [];
   displayedColumns: Array<String> = ['type', 'count'];
 
+  node_size = {
+    height: 80,
+    width: 170
+  };
+  layoutSettingsVertical = {
+    orientation: 'TB'
+  };
+
   diagram_graph = {
     nodes: [
       {
@@ -19,40 +27,46 @@ export class AboutComponent implements OnInit {
         data: {
           colour: '#0084b4',
           textColour: 'white'
-        }
+        },
+        dimension: this.node_size
       }, {
         id: 'ext_cred',
         label: 'External Assessments',
         data: {
           colour: '#db5139',
           textColour: 'white'
-        }
+        },
+        dimension: {...this.node_size, ...{width: 350}}
       }, {
         id: 'fc',
         label: 'Fact-Checkers',
         data: {
           colour: '#4caf50',
           textColour: 'white'
-        }
+        },
+        dimension: {...this.node_size, ...{width: 250}}
       }, {
         id: 'credibility',
         label: 'Credibility Model',
         data: {
           colour: '#f5d442'
-        }
+        },
+        dimension: {...this.node_size, ...{width: 250}}
       }, {
         id: 'backend',
         label: 'Backend',
         data: {
           colour: '#6BBBAE'
-        }
+        },
+        dimension: this.node_size
       }, {
         id: 'frontend',
         label: 'Frontend',
         data: {
           colour: '#693C5E',
           textColour: 'white'
-        }
+        },
+        dimension: this.node_size
       }
     ],
     links: [{
