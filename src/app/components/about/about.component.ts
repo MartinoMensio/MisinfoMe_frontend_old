f2aabf2b177788e627a1cc222ac15e7f20fb358b
Martino Mensio <martino.mensio@open.ac.uk>
2019-09-05 17:23:09 +0100

    update about page

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -11,12 +11,85 @@ export class AboutComponent implements OnInit {
   about_stats = [];
   displayedColumns: Array<String> = ['type', 'count'];
 
+  diagram_graph = {
+    nodes: [
+      {
+        id: 'twitter',
+        label: 'Twitter',
+        data: {
+          colour: '#0084b4',
+          textColour: 'white'
+        }
+      }, {
+        id: 'ext_cred',
+        label: 'External Assessments',
+        data: {
+          colour: '#db5139',
+          textColour: 'white'
+        }
+      }, {
+        id: 'fc',
+        label: 'Fact-Checkers',
+        data: {
+          colour: '#4caf50',
+          textColour: 'white'
+        }
+      }, {
+        id: 'credibility',
+        label: 'Credibility Model',
+        data: {
+          colour: '#f5d442'
+        }
+      }, {
+        id: 'backend',
+        label: 'Backend',
+        data: {
+          colour: '#6BBBAE'
+        }
+      }, {
+        id: 'frontend',
+        label: 'Frontend',
+        data: {
+          colour: '#693C5E',
+          textColour: 'white'
+        }
+      }
+    ],
+    links: [{
+      id: 'a',
+      source: 'frontend',
+      target: 'backend',
+      label: 'REST'
+    }, {
+      id: 'b',
+      source: 'backend',
+      target: 'twitter',
+      label: 'REST'
+    }, {
+      id: 'c',
+      source: 'backend',
+      target: 'credibility',
+      label: 'REST'
+    }, {
+      id: 'd',
+      source: 'credibility',
+      target: 'fc',
+      label: 'DATA COLLECTION'
+    }, {
+      id: 'e',
+      source: 'credibility',
+      target: 'ext_cred',
+      label: 'DATA COLLECTION'
+    }
+  ]
+  };
+
   constructor(private apiService: APIService) { }
 
   ngOnInit() {
     this.apiService.getStats().subscribe(res => {
       this.about_stats = Object.keys(res).reduce((acc: Array<any>, cur) => {
-        acc.push({'type': cur, 'count': res[cur]});
+        acc.push({ 'type': cur, 'count': res[cur] });
         return acc;
       }, []);
       console.log('hola');
